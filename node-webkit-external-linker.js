443beec32ba5a508da05e422dd26eeb0d4eb9c42
Eric Newport <kethinov@gmail.com>
2014-12-08 14:15:26 -0500

    refactoring

--- node-webkit-external-linker.js
+++ node-webkit-external-linker.js
@@ -2,18 +2,20 @@
   var gui = require('nw.gui');
 
   function externalLinker(e) {
-    var link = e.target, rel, target = link.target, href = link.href;
+    var link = e.target,
+        rel, // declared now, defined later
+        target = link.target,
+        href = link.href;
 
-    if (!link || !link.getAttribute || !href) {
+    // abort link hijack if crucial variables are not defined or if user sets link to target='_self'
+    if (!link || !link.getAttribute || !href || target === '_self') {
       return;
     }
 
+    // defined now that we know that link.getAttribute is not undefined
     rel = link.getAttribute('rel');
 
-    if (target === '_self') {
-      return;
-    }
-
+    // hijack the link and open in a new window under these conditions
     if (rel === 'external' || target === '_blank' || target === '_top' || href.substr(0, 4) === 'http') {
       e.preventDefault();
       gui.Shell.openExternal(href);
